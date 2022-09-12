0d645e001738c040dca2b4591e734af3fa63c396
olalekanowolabi64 <39282329+olalekanowolabi64@users.noreply.github.com>
2022-09-12 10:52:48 +0100

    Add files via upload

---
+++ topicmodel_dis.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 10:45:56 2022
+
+@author: HP
+"""
+
+#pip install pyLDAvis -qq
+# Import required packages
+import numpy as np
+import logging
+import pyLDAvis
+import pyLDAvis.gensim
+#import pyLDAvis.gensim_models
+import json
+import warnings
+import pandas as pd
+import nltk
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+warnings.filterwarnings('ignore')  # To ignore all warnings that arise here to enhance clarity
+
+from gensim.models.coherencemodel import CoherenceModel
+from gensim.models.ldamodel import LdaModel
+from gensim.corpora.dictionary import Dictionary
+from numpy import array
+
+# Import dataset
+p_df = pd.read_csv("C:/Users/HP/realvascu7.csv")
+
+# Convert to array
+docs =array(p_df['text'])
+# Define function for tokenize and lemmatizing
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
+
+def docs_preprocessor(docs):
+    tokenizer = RegexpTokenizer(r'\w+')
+    for idx in range(len(docs)):
+        docs[idx] = docs[idx].lower()  # Convert to lowercase.
+        docs[idx] = tokenizer.tokenize(docs[idx])  # Split into words.
+
+    # Remove numbers, but not words that contain numbers.
+    docs = [[token for token in doc if not token.isdigit()] for doc in docs]
+    
+    # Remove words that are only one character.
+    docs = [[token for token in doc if len(token) > 3] for doc in docs]
+    
+    # Lemmatize all words in documents.
+    lemmatizer = WordNetLemmatizer()
+    docs = [[lemmatizer.lemmatize(token) for token in doc] for doc in docs]
+  
+    return docs
+# Perform function on our document
+docs = docs_preprocessor(docs)
+#Create Biagram & Trigram Models 
+from gensim.models import Phrases
+# Add bigrams and trigrams to docs,minimum count 10 means only that appear 10 times or more.
+bigram = Phrases(docs, min_count=10)
+trigram = Phrases(bigram[docs])
+
+for idx in range(len(docs)):
+    for token in bigram[docs[idx]]:
+        if '_' in token:
+            # Token is a bigram, add to document.
+            docs[idx].append(token)
+    for token in trigram[docs[idx]]:
+        if '_' in token:
+            # Token is a bigram, add to document.
+            docs[idx].append(token)
+#Remove rare & common tokens 
+# Create a dictionary representation of the documents.
+dictionary = Dictionary(docs)
+dictionary.filter_extremes(no_below=10, no_above=0.2)
+#Create dictionary and corpus required for Topic Modeling
+corpus = [dictionary.doc2bow(doc) for doc in docs]
+print('Number of unique tokens: %d' % len(dictionary))
+print('Number of documents: %d' % len(corpus))
+print(corpus[:1])
+
+# Set parameters.
+num_topics = 35
+chunksize = 500 
+passes = 20 
+iterations = 400
+eval_every = 1  
+
+# Make a index to word dictionary.
+temp = dictionary[0]  # only to "load" the dictionary.
+id2word = dictionary.id2token
+
+lda_model = LdaModel(corpus=corpus, id2word=id2word, chunksize=chunksize, \
+                       alpha='auto', eta='auto', \
+                       iterations=iterations, num_topics=num_topics, \
+                       passes=passes, eval_every=eval_every)
+# Print the Keyword in the 5 topics
+print(lda_model.print_topics())
+
+# Compute Coherence Score using c_v
+coherence_model_lda = CoherenceModel(model=lda_model, texts=docs, dictionary=dictionary, coherence='c_v')
+coherence_lda = coherence_model_lda.get_coherence()
+print('\nCoherence Score: ', coherence_lda)
+
+# Compute Coherence Score using UMass
+coherence_model_lda = CoherenceModel(model=lda_model, texts=docs, dictionary=dictionary, coherence="u_mass")
+coherence_lda = coherence_model_lda.get_coherence()
+print('\nCoherence Score: ', coherence_lda)
+
+def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
+    """
+    Compute c_v coherence for various number of topics
+
+    Parameters:
+    ----------
+    dictionary : Gensim dictionary
+    corpus : Gensim corpus
+    texts : List of input texts
+    limit : Max num of topics
+
+    Returns:
+    -------
+    model_list : List of LDA topic models
+    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+    """
+    coherence_values = []
+    model_list = []
+    for num_topics in range(start, limit, step):
+        model=LdaModel(corpus=corpus, id2word=dictionary, num_topics=num_topics)
+        model_list.append(model)
+        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
+        coherence_values.append(coherencemodel.get_coherence())
+
+    return model_list, coherence_values
+
+model_list, coherence_values = compute_coherence_values(dictionary=dictionary, corpus=corpus, texts=docs, start=2, limit=40, step=6)
+# Show graph
+import matplotlib.pyplot as plt
+limit=40; start=2; step=6;
+x = range(start, limit, step)
+plt.plot(x, coherence_values)
+plt.xlabel("Num Topics")
+plt.ylabel("Coherence score")
+plt.legend(("coherence_values"), loc='best')
+plt.show()
+
+lda_model = LdaModel(corpus=corpus, id2word=dictionary, iterations=100, num_topics=8)
+lda_model.print_topics(-1)
+lda_display = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary)
+pyLDAvis.display(lda_display)
+
+
+def format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=docs):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row_list in enumerate(ldamodel[corpus]):
+        row = row_list[0] if ldamodel.per_word_topics else row_list            
+        # print(row)
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return(sent_topics_df)
+
+
+df_topic_sents_keywords = format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=docs)
+
+# Format
+df_dominant_topic = df_topic_sents_keywords.reset_index()
+df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+df_dominant_topic.head(10)
+
+
+
+# Display setting to show more characters in column
+pd.options.display.max_colwidth = 100
+
+sent_topics_sorteddf_mallet = pd.DataFrame()
+sent_topics_outdf_grpd = df_topic_sents_keywords.groupby('Dominant_Topic')
+
+for i, grp in sent_topics_outdf_grpd:
+    sent_topics_sorteddf_mallet = pd.concat([sent_topics_sorteddf_mallet, 
+                                             grp.sort_values(['Perc_Contribution'], ascending=False).head(1)], 
+                                            axis=0)
+
+# Reset Index    
+sent_topics_sorteddf_mallet.reset_index(drop=True, inplace=True)
+
+# Format
+sent_topics_sorteddf_mallet.columns = ['Topic_Num', "Topic_Perc_Contrib", "Keywords", "Representative Text"]
+
+# Show
+sent_topics_sorteddf_mallet.head(10)
+
+
+# 1. Wordcloud of Top N words in each topic
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+cloud = WordCloud(stopwords=STOPWORDS,
+                  background_color='white',
+                  width=2500,
+                  height=1800,
+                  max_words=10,
+                  colormap='tab10',
+                  color_func=lambda *args, **kwargs: cols[i],
+                  prefer_horizontal=1.0)
+
+topics = lda_model.show_topics(formatted=False)
+
+fig, axes = plt.subplots(2,4, figsize=(10,10), sharex=True, sharey=True)
+
+for i, ax in enumerate(axes.flatten()):
+    fig.add_subplot(ax)
+    topic_words = dict(topics[i][1])
+    cloud.generate_from_frequencies(topic_words, max_font_size=300)
+    plt.gca().imshow(cloud)
+    plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+    plt.gca().axis('off')
+
+
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.axis('off')
+plt.margins(x=0, y=0)
+plt.tight_layout()
+plt.show()
